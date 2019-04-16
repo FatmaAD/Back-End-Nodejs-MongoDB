3d8582496ee38f104f2de9aa55da26da4b187276
fatma Alaa <fatma@iti.com>
2019-04-16 23:12:12 +0200

    connection string

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 mongoose.connect(
-  "mongodb+srv://fatma:2314876abc@cluster0-xph4u.mongodb.net/test?retryWrites=true",
+    
+//   "mongodb+srv://fatma:2314876abc@cluster0-xph4u.mongodb.net/test?retryWrites=true",
+ "mongodb+srv://fatma:2314876abc@cluster0-xph4u.mongodb.net/test",
   {
     useNewUrlParser: true,
     useCreateIndex: true
