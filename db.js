fa367aba383414a2e9134c510518a3df2c38f926
fatma Alaa <fatma@iti.com>
2019-04-16 22:52:14 +0200

    node config

--- db.js
+++ db.js
@@ -1,5 +1,8 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/ecommerce', {
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://fatma:2314876abc@cluster0-xph4u.mongodb.net/test?retryWrites=true",
+  {
     useNewUrlParser: true,
-    useCreateIndex: true,
-})
+    useCreateIndex: true
+  }
+);
